525d501aa3bf4bf6b3b4b9307dfde46f31b27245
Gael Varoquaux <gael.varoquaux@normalesup.org>
2019-09-17 16:38:27 -0400

    Multiple displays

--- plot_intuitions.py
+++ plot_intuitions.py
@@ -61,12 +61,27 @@ Z_2 = np.reshape(kernel_2, T_1.shape)
 
 Z_diff = np.abs(Z_1 - Z_2)
 
+def save(name):
+    ax.set_xlim([z1min, z1max])
+    ax.set_ylim([z2min, z2max])
+    plt.axis("off")
+    plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    fig.savefig("%s.jpg" % name)
+    fig.savefig("%s.pdf" % name)
+
 
 fig, ax = plt.subplots(figsize=(4, 2.5))
+ax.plot(X[:, 0], X[:, 1], "o", color="C0", markersize=4, label="P")
+ax.plot(Y[:, 0], Y[:, 1], "x", color="C1", markersize=5, label="Q")
+
+save('plot_contour_1')
+
 contours_1 = plt.contour(T_1, T_2, Z_1, 2, colors="C0", linestyles="-.",
                          linewidths=2)
 contours_2 = plt.contour(T_1, T_2, Z_2, 2, colors="C1", linestyles="-.",
                          linewidths=2)
+save('plot_contour_2')
+
 #img = plt.pcolormesh(
 #    T_1, T_2, Z_diff, cmap="gray_r")
 # imshow code rather than pcolormesh: it produces better figures
@@ -80,13 +95,6 @@ contours_3 = plt.contour(
     T_1, T_2, Z_diff, 2, colors="k", linestyles="-", linewidths=.8,
 )
 #plt.clabel(contours_3, inline=True, fontsize=8)
-ax.plot(X[:, 0], X[:, 1], "o", color="C0", markersize=4, label="P")
-ax.plot(Y[:, 0], Y[:, 1], "x", color="C1", markersize=5, label="Q")
-ax.set_xlim([z1min, z1max])
-ax.set_ylim([z2min, z2max])
-plt.axis("off")
-plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
+save('plot_contour')
 plt.show()
-fig.savefig("plot_contour.jpg")
-fig.savefig("plot_contour.pdf")
 

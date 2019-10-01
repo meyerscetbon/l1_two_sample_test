6aa01a421135c67e4e44db3a295fc5e129a23c63
SCETBON Meyer <meyer.scetbon@inria.fr>
2019-10-01 09:22:25 -0700

    plot

--- example_simple.py
+++ example_simple.py
@@ -13,6 +13,13 @@ mean_shift = np.zeros(dim)
 mean_shift[0] = 1
 Y = np.random.multivariate_normal(mean_shift, np.eye(dim), 2 * num_sample_test)
 
+# Plot the data
+plt.plot(X[:, 0], X[:, 1], "b.", markeredgecolor="b", label="$X\sim P$", alpha=0.5)
+plt.plot(Y[:, 0], Y[:, 1], "r.", markeredgecolor="r", label="$Y\sim Q$", alpha=0.5)
+plt.axis("equal")
+plt.legend()
+plt.show()
+
 # Split X, Y into train and test sets
 Itr = np.zeros(2 * num_sample_test, dtype=bool)
 tr_ind = np.random.choice(2 * num_sample_test, int(num_sample_test), replace=False)
@@ -22,12 +29,6 @@ Ite = np.logical_not(Itr)
 X_tr, Y_tr = X[Itr, :], Y[Itr, :]
 X_te, Y_te = X[Ite, :], Y[Ite, :]
 
-# Plot the data
-plt.plot(X[:, 0], X[:, 1], "b.", markeredgecolor="b", label="$X\sim P$", alpha=0.5)
-plt.plot(Y[:, 0], Y[:, 1], "r.", markeredgecolor="r", label="$Y\sim Q$", alpha=0.5)
-plt.axis("equal")
-plt.legend()
-plt.show()
 
 # L1_opt_J_ME
 test_locs, gwidth2 = l1_two_sample_test.solver_ME(X_tr, Y_tr)
